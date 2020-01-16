a082c5bc11232159316b0eb0abefa0b4c8005bf9
Tarang Hirani <tarang9211@gmail.com>
2020-01-16 11:59:27 +1100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,81 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react"
+import data from "../data"
+import TwitterIcon from "../public/assets/svgs/twitter.svg"
+import GithubIcon from "../public/assets/svgs/github.svg"
+import LinkedInIcon from "../public/assets/svgs/linkedin.svg"
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+const Index = () => {
+  return (
+    <React.Fragment>
+      <div className="container mx-auto">
+        <section className="min-w-full flex justify-center">
+          <div className=" profile-bg bg-cover bg-no-repeat bg-center border-solid border-2 border-gray-100 rounded-full shadow-2xl w-40 h-40" />
+        </section>
+        <section className="mt-10">
+          <h4 className="font-sans text-center antialiased font-medium text-3xl text-gray-100 mb-0">
+            Hi, I'm Tarang{" "}
+            <span className="text-3xl" role="img" aria-label="emoji">
+              🙋‍♂️
+            </span>
+          </h4>
+          <h5 className="font-sans text-center antialiased font-medium text-xl text-gray-200 mt-4 mb-0">
+            Full Stack Engineer
+          </h5>
+          <div className="flex justify-center mt-4">
+            <div className="w-5 h-5 mr-4">
+              <a
+                aria-label="Twitter"
+                href="https://twitter.com/tarang9211"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TwitterIcon />
+              </a>
+            </div>
+            <div className="w-5 h-5 mr-4">
+              <a
+                aria-label="Github"
+                href="https://github.com/tarang9211"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GithubIcon />
+              </a>
+            </div>
+            <div className="w-5 h-5">
+              <a
+                aria-label="LinkedIn"
+                href="https://www.linkedin.com/in/tarang-hirani-72a9b83a/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedInIcon />
+              </a>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h4 className="underline italic font-sans antialiased font-medium text-xl text-gray-300 mb-6 mt-6">
+            Experience
+          </h4>
+          <ul className="p-0 list-none">
+            {data.map(d => {
+              return (
+                <li key={JSON.stringify(d)} className="mb-10">
+                  <p className="font-sans antialiased text-base mb-1 font-medium text-gray-200">
+                    {d.title}
+                  </p>
+                  <p className="font-sans antialiasted text-sm m-0 text-gray-500">
+                    {d.subtitle}
+                  </p>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
       </div>
-    </div>
+    </React.Fragment>
+  )
+}
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Index;
