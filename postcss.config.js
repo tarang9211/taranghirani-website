ed8d671d15cfbdce6d232fdb5628ba6d8f98f505
Tarang Hirani <tarang9211@gmail.com>
2025-05-25 20:49:41 +0530

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
+// postcss.config.js
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
-}
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
