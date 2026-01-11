d761879b0407983d5eacc11e51fcae083c7b3271
Tarang Hirani <tarang9211@gmail.com>
2026-01-11 18:00:51 +0530

    fix next script error

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import Script from "next/script";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+
+const GA_MEASUREMENT_ID = "G-JBM6SPGXWS";
 
 export default function MyApp({ Component, pageProps }) {
   const meta = {
@@ -22,15 +25,19 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JBM6SPGXWS"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-JBM6SPGXWS');
-        </script>
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
